298b9f9ca4952c224978ed573129ea7d08ad4be0
Mahmud2640 <mdmahmud484@gmail.com>
2022-05-01 04:50:44 +0600

    backend database update

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import "./AddUser.css";
 
 const AddUser = () => {
-   return (
-      <div>
-         <h1>Adding Users!!!!!</h1>
-      </div>
-   );
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+
+    // send data server
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+      alert("User added successfully");
+      e.target.reset();
+  };
+  return (
+    <div>
+      <form onSubmit={handleAddUser}>
+        <h3>Add User</h3>
+        <label for="name">name</label>
+        <input type="text" placeholder="Enter Username" id="name" required />
+        <label for="email">Email</label>
+        <input type="email" placeholder="Enter Email" id="email" required />
+
+        <div class="btn from-top">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddUser;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-   return (
-      <div>
-         <h1>Home</h1>
-      </div>
-   );
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const deleteUser = (id) => {
+    const proceed = window.confirm(
+      "Are you sure you want to delete this user?"
+    );
+    if (proceed) {
+      fetch(`http://localhost:5000/user/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Available Users = {users.length}</h1>
+
+      {users.map((user) => (
+        <div key={user._id}>
+          <h1>{user.name}</h1>
+          <h2>{user.email}</h2>
+          <button onClick={() => deleteUser(user._id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
